742ba9da0c9d41d226b65bfa189d6cc28b3372a1
sadanora <sadanora.pi@gmail.com>
2023-09-01 14:43:24 +0900

    中間テーブルを追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,10 @@ class Book < ApplicationRecord
     # scope :find_price, ->(price) { find_by(price: price) }
 
     belongs_to :publisher
+    has_many :book_authors
+    has_many :authors, through: :book_authors
+
+    validates :name, presence: true
+    validates :name, length: { maximum: 25 }
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
