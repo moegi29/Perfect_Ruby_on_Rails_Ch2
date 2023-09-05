1b4527f392411c8bf6724977aa05f51d2622e0d5
sadanora <sadanora.pi@gmail.com>
2023-09-05 15:40:47 +0900

    sales_statusカラムを追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
+    enum sales_status: {
+        reservation: 0, #予約受付
+        now_on_sale: 1, #販売中
+        end_of_print: 2, #販売終了
+    }
+
     scope :costly, -> { where("price > ?", 3000) }
     scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
     # scope :find_price, ->(price) { find_by(price: price) }
@@ -15,7 +21,12 @@ class Book < ApplicationRecord
             "lovely #{matched}"
         end
     end
-    after_destroy do
-        Rails.logger.info "Book is deleted: #{self.attributes}"
+    after_destroy :if => :high_price? do
+        Rails.logger.warn "Book with high price is deleted: #{self.attributes}"
+        Rails.logger.warn "Please check!!"
+    end
+
+    def high_price?
+        price >= 5000
     end
 end
