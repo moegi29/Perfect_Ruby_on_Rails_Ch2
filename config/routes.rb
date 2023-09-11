cf7f47275fad184f6504c903c4a96e74207c38c6
motohiro-mm <polarbear.dx@gmail.com>
2023-09-11 15:41:10 +0900

    Userモデルの追加、Profilesコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   get "/books/:id" => "books#show"
   delete "/books/:id" => "books#destroy"
+  resources :publishers
+  resource :profile, only: %i{show edit update}
 end
