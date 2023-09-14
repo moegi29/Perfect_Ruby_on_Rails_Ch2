8814ba3ce041726f66627ab15a624f30a8fc9649
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-09-14 15:40:12 +0900

    variantsによるテンプレート切り替えを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
 #  before_action do
 #    redirect_to access_denied_path if params[:token].blank?
 #  end
+
+  before_action :detect_mobile_variant
+
+  private
+
+  def detect_mobile_variant
+    request.variant = :mobile if request.user_agent =~ /iPhone/
+  end
 end
